9654a815b593c89054c33b2ff3f168f4f226f454
up201302892 <up201302892@B02AY0606.fe.up.pt>
2016-11-11 15:15:05 +0000

    ServerBL quase

--- BattlePirateShipServer/src/BusinessLogic/ServerBL.java
+++ BattlePirateShipServer/src/BusinessLogic/ServerBL.java
@@ -18,39 +18,94 @@ import javax.swing.JOptionPane;
  *
  * @author Vitor
  */
-public class ServerBL {
-    private String cmd;
-    
-     public ServerBL(String cmd) {
-      this.cmd = cmd;
-    }
 
-    public int login(String user, String pass, Connection connection) 
+
+public class ServerBL 
+{
+        public ServerBL() 
+        {
+            //constructor method
+        }
+
+    public int login(String user, String pass, Connection connection) throws SQLException 
     {
-        Statement stmt = null;
-        int uid=0;
+        // This method checks if there is an user in the database with name user
+        // and password pass. it returns 1 on sucess, -1 if there is no user with
+        // that name in the database and -2 if we have an sql error while checking
+        // the database
+        
+        // User: Username of the user trying to login
+        // Pass: Password of the user trying to login
+        // Connection: Variable to access the database
+        
+        //Variables for database acess
+        Statement stmt = null;  
+       
+        //varibles to save the data we extract from the database
+        String username;
+        String password;
+        int uid;
+        
+        //query we are executing on the database
         String query = "select * " + "from login";
+        
+        //accessing the database
         try 
         {
             stmt = connection.createStatement();
             ResultSet rs = stmt.executeQuery(query);
-            while (rs.next()) 
+            
+            //going through the whole table in database
+            while ( rs.next() ) 
             {
+                //getting the id, username and password
                 uid = rs.getInt("user_id");
-                String username = rs.getString("username");
-                String password = rs.getString("password");
+                username = rs.getString("username");
+                password = rs.getString("password");
+                
+                //comparing the results to see if the user is in the database
                 if( username.equals(user) && password.equals(pass) )
                 {
-                    //JOptionPane.showMessageDialog(null,"User found! UID:"+uid+" \n Username:"+username+"");
+                    // We found the user in the database
                     stmt.close();
                     return 1;
                 }
             }
-         //JOptionPane.showMessageDialog(null,"Username/password wrong! Not found on DB!");
+            // Didn't find user in database, now we continue 
         } 
         catch (SQLException e ) 
         {
-        //JOptionPane.showMessageDialog(null,"SQL Exception");
+            //we had an sql problem, and return and error code
+            if (stmt != null)
+            {
+                stmt.close(); 
+            }
+            return -2;
+        } 
+        finally 
+        {
+            //no sql problem, so we return -11 to signal the user wasn't found in the db
+            if (stmt != null) 
+            { 
+              stmt.close();
+            }
+        }
+        return -1;
+    }   
+    
+    public int register(String user, String pass, Connection connection) 
+    {
+        Statement stmt = null;
+        String query = "INSERT INTO login " + "VALUES (DEFAULT,'"+ user +"','"+ pass +"')";
+        try 
+        {
+            stmt = connection.createStatement();
+            stmt.executeUpdate(query);
+        } 
+        catch (SQLException e ) 
+        {
+            JOptionPane.showMessageDialog(null,"SQL Exception");
+            return 0;
         } 
         finally 
         {
@@ -58,48 +113,16 @@ public class ServerBL {
             { 
                 try 
                 {
+                    JOptionPane.showMessageDialog(null," User:"+user+"\n Password:"+pass+" \n Success!");
                     stmt.close();
                 } 
                 catch (SQLException ex) 
                 {
-               
-                }
-            }
-        }
-        return 1;
-    }   
-    
-    public int register(String user, String pass, Connection connection) 
-{
-    Statement stmt = null;
-    String query = "INSERT INTO login " +
-                   "VALUES (DEFAULT,'"+ user +"','"+ pass +"')";
-    try {
-       stmt = connection.createStatement();
-       stmt.executeUpdate(query);
-    } catch (SQLException e ) {
-        JOptionPane.showMessageDialog(null,"SQL Exception");
-        return 0;
-    } 
-    finally 
-    {
-        if (stmt != null) 
-        { 
-            try {
-                JOptionPane.showMessageDialog(null," User:"+user+"\n Password:"+pass+" \n Success!");
-                stmt.close();
-                } 
-            catch (SQLException ex) 
-                {
-               
                 return 0;
                 }
-        } 
-    return 1;
-    }
-   
-}  
-
-}  
-
+            } 
+        return 1;
+        }
+    }   
+}
 
