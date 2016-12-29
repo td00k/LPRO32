d621fc0cb161f8fa18b4cd74a1113cb728db922f
tiagodias <tiagodias@wifi-interno.fe.up.pt>
2016-12-29 18:45:30 +0000

    ja ta a dar mais counter

--- BattlePirateShipClient/src/BusinessLogic/User.java
+++ BattlePirateShipClient/src/BusinessLogic/User.java
@@ -5,6 +5,8 @@ package BusinessLogic;
      * This class handles user information to send to interface.
      */
 
+import Communications.PirateProtocol;
+        
 public class User 
 {
   private static int userid; 
@@ -16,16 +18,27 @@ public class User
   private static int gamesplayed; 
   private static int surrenders; 
   
+  PirateProtocol pirate;
   public User()
   {
       
+      pirate = new PirateProtocol();
+      
   }
   
-  public String[] get()
+  public String[] get(int userid)
   {
-      String[] info;
-      info = new String[20];
-      return info;
+      String[] tosend = new String[1];
+      tosend[0] = Integer.toString(userid);
+      
+      return pirate.run(8,tosend,1);
   }
     
+  public String[] getFriends(int userid)
+  {
+    String[] tosend = new String[1];
+    tosend[0] = Integer.toString(userid);
+      
+    return pirate.run(10,tosend,1);
+  }    
 }
